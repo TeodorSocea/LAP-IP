af4f260da177886519226cc0d3ea1130b62ad6b3
TeodorSocea <teodor.socea@gmail.com>
2022-03-21 14:27:15 +0200

    Initial commit

---
+++ setup.bat
@@ -0,0 +1,7 @@
+echo "# LAP-IP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:TeodorSocea/LAP-IP.git
+git push -u origin main
